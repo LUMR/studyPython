66f5554ca0ec2c3bf9cc68cf34c0022ac286eb94
luwc <wencheng.lu@meicloud.com>
2018-07-16 17:30:53 +0800

    网易转换

--- src/netease.py
+++ src/netease.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import sys,os
+import sys, os
 
 
 def change_file(filepath):
@@ -15,9 +15,26 @@ def change_file(filepath):
             print('转换完成:%d' % cou)
 
 
+def change_file1(source, targe):
+    with open(source, 'rb') as file:
+        print('打开文件%s ' % file.name)
+        with open(targe, 'wb') as nfile:
+            print('新文件 %s' % nfile.name)
+            b = file.read()
+            a = bytes([by ^ 0xA3 for by in b])
+            nfile.write(a)
+            print('转换完成')
+
+
 if __name__ == '__main__':
-    files = sys.argv[1:]
-    for filepath in files:
-        change_file(filepath)
-
-
+    args = sys.argv[1:]
+    if args.__len__() < 2:
+        change_file(args[0])
+    else:
+        source = args[0]
+        targe = args[1]
+        files = os.listdir(source)
+        for file in files:
+            fileName = os.path.splitext(file)
+            if fileName[1] == '.uc!':
+                change_file1(os.path.join(source, file), os.path.join(targe, fileName[0] + '.mp3'))
