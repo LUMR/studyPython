b8967d8bfab2315408f5344e83c4a2486f94c65f
lumr <freedomyes@sina.com>
2019-09-28 12:03:57 +0800

    图像搜索

--- src/bilanauto.py
+++ src/bilanauto.py
@@ -4,7 +4,14 @@ from subprocess import call
 import cv2
 
 import pykeyboard
-import pymouse
+from pymouse import PyMouse
+
+# 图片路径
+PATH = '/Users/lumr/tmp/bl/'
+# 截图尺寸与鼠标移动尺寸比例
+R = 2
+
+fileName = PATH + 'enter.jpg'
 
 
 class MouseController:
@@ -30,17 +37,24 @@ class MouseController:
         self.m.release(x, y, 2)
 
 
-fileName = '/Users/lumr/tmp/test.jpg'
+def findImgLocation(srcPath, templatePath):
+    src = cv2.imread(PATH + srcPath, 0)
+    temp = cv2.imread(PATH + templatePath, 0)
+    res = cv2.matchTemplate(src, temp, cv2.TM_CCORR_NORMED)
+    h, w = temp.shape[0:2]
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+    return (max_loc[0] + w / 2) / R, (max_loc[1] + h / 2) / R
+
 
 if __name__ == '__main__':
-    call(['screencapture', '-tjpg', '/Users/lumr/tmp/test.jpg'])
-    cv2.imread(fileName)
-    m = MouseController(pymouse.PyMouse())
-    k = pykeyboard.PyKeyboard()
-    # print('鼠标当前位置是：%s,%s' % m.position())
-    m.click(600, 500, 2)
-
-    # m.click(600, 500, 2, 2)
-    # m.press(720, 450, 2)
-    time.sleep(0.1)
-    # m.release(720, 450, 2)
+    # call(['screencapture', '-tjpg', fileName])
+    gScreen = cv2.imread(PATH + 'enter.jpg', 0)
+    screenHeight, screenWidth = gScreen.shape[0:2]
+    pbutton = cv2.imread(PATH + 'enterbutton.jpg', 0)
+    res = cv2.matchTemplate(gScreen, pbutton, cv2.TM_CCORR_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+    print('max value is {0} pos is {1}'.format(max_val, max_loc))
+    x, y = findImgLocation('enter.jpg', 'enterbutton.jpg')
+    print('x = {0} , y = {1}'.format(x, y))
+    m = MouseController(PyMouse())
+    m.rclick(x, y)
